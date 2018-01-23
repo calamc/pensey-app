46b30d61cec0d3d9b7a1aba965911c1aafb21fa3
calamc <cmmacmac@gmail.com>
2018-01-23 23:33:52 +0000

    Added numeraljs for amount formatting and locales

--- src/components/ExpListItem.js
+++ src/components/ExpListItem.js
@@ -5,10 +5,18 @@ import { connect } from 'react-redux'
 // Link tag for reactrouter v4
 import { Link } from 'react-router-dom'
 
+import numeral from 'numeral'
+import moment from 'moment'
+import '../locales/locales'
+
 const ExpListItem = ({id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}><h1>{description}</h1></Link>
-        <p>{amount} / {createdAt}</p>
+        <p>
+            <span>€</span>{numeral(amount / 100).format('0,0.00')} 
+                / 
+            {moment(createdAt).format('DD MMMM YY')}
+        </p>
     </div>
 );
 
---
+++ src/locales/locales.js
@@ -0,0 +1,22 @@
+// // load a locale
+// numeral.register('locale', 'en', {
+//     delimiters: {
+//         thousands: ' ',
+//         decimal: ','
+//     },
+//     abbreviations: {
+//         thousand: 'k',
+//         million: 'm',
+//         billion: 'b',
+//         trillion: 't'
+//     },
+//     ordinal : function (number) {
+//         return number === 1 ? 'er' : 'ème';
+//     },
+//     currency: {
+//         symbol: '€'
+//     }
+// });
+
+// // switch between locales
+// numeral.locale('en');
