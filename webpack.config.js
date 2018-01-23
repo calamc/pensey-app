d36356f12be985ecb750f50897401ed65aafee21
calamc <cmmacmac@gmail.com>
2018-01-23 21:07:30 +0000

    Set up production env for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports ={
+
+module.exports = (env) => {
+    const cssExtract = new ExtractTextPlugin('styles.css');
+    const isProd = env === 'prod';
+    
+    console.log('env', env);
+    return {
     entry: './src/app.js',
     output: {
         path:path.join(__dirname, 'public'),
@@ -14,16 +20,31 @@ module.exports ={
             exclude: /node_modules/
         }, {
             test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+            use: cssExtract.extract({
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap:true
+                        }
+                    },
+                    {
+                        loader:'sass-loader',
+                        options: {
+                            sourceMap:true
+                        }
+                    }  
+                ]
+            })
         }]
     },
-    devtool:'cheap-module-eval-source-map',
+    plugins: [
+        cssExtract
+    ],
+    devtool: isProd ? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
+        }
     }
 };
