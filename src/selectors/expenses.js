3097538a70321d096540bc919650eb4148972e6e
calamc <cmmacmac@gmail.com>
2018-04-29 21:37:15 +0100

    Fixed a bug with textMatch for search filter

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -5,10 +5,10 @@ export default (expenses, { text, sortBy, startDate, endDate}) => {
         const createdAtTime = moment(expense.createdAt); // get created at datetime
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtTime, 'day') : true; // ternary op - true/false
         const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtTime, 'day') : true; // ternary op - true/false
-        //const textMatch = expense.title.toLowerCase().includes(text.toLowerCase());
+        const textMatch = expense.title.toLowerCase().includes(text.toLowerCase());
 
-        // return startDateMatch && endDateMatch && textMatch;
-        return startDateMatch && endDateMatch;
+        return startDateMatch && endDateMatch && textMatch;
+        //return startDateMatch && endDateMatch;
     }).sort((a,b) => {
         if (sortBy === 'date') {
             return a.createdAt < b.createdAt ? 1 : -1; // ascending or descending sort order
