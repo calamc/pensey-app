61c323c1375f8d6b675649dcf391cb3008b5082d
calamc <cmmacmac@gmail.com>
2018-05-01 22:55:35 +0100

    Updated explinechart and analytics component

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -3,11 +3,61 @@ import { Pie } from 'react-chartjs-2'
 import {connect} from 'react-redux'
 
 import moment from 'moment'
+import numeral from 'numeral'
 
-export const Analytics = () => (
-    <div>
-        Analytics view
-    </div>
-);
+import selectExpenses from '../selectors/expenses'
+import selectExpensesTtl from '../selectors/expensesTtl'
 
-export default Analytics;
+export const Analytics = ({expCount, expTtl, expenses}) => {
+
+    const data = {
+        datasets: [{
+            data: [20, 40, 40]
+        }],
+        labels: [
+            'Travel',
+            'Meeting',
+            'Food and Drink'
+        ]
+    }
+
+    const options = {
+        legend: {
+            display: true,
+        },
+        cutoutPercentage: 20
+    }
+
+    const totalAmount = numeral(expTtl / 100).format('€0,0.00');
+
+    return (
+        <div className="content">
+            <div className="header-analytics">
+                <h1 className="dash-header__title"><span>Analytics</span></h1>
+            </div>
+            <div className="content">
+                <h1 className="dash-header__title">You have added <span className="dash-header__s">{expCount}</span> expenses in total since you created your account.</h1>
+                <h1 className="dash-header__title">The combined total of all your expenses is <span className="dash-header__s">€{totalAmount}</span></h1>
+            </div>
+            <div className="content">
+            <hr/>
+                <h1 className="dash-header__title">Category view</h1>
+                <Pie data={data} options={options} redraw/>
+            </div>
+        </div>
+    )
+};
+
+const mapStateToProps = (state) => {
+    const allExpenses = selectExpenses(state.expenses, state.filters);
+
+    return {
+        expCount: allExpenses.length,
+        expTtl: selectExpensesTtl(allExpenses),
+        expHidden: state.expenses.length - allExpenses.length,
+        expenses: selectExpenses(state.expenses, state.filters)
+        
+    };
+};
+
+export default connect(mapStateToProps)(Analytics);
--- src/components/ExpLineChart.js
+++ src/components/ExpLineChart.js
@@ -4,75 +4,97 @@ import {connect} from 'react-redux'
 
 import moment from 'moment'
 
-// gather chartjs data
-const data = {
-    datasets: [
-                {
-                    // styling for line chart using chartjs api props
-                    label: 'Filtered Expenses',
-                    showLine: true, // draw line relating to the data
-                    fill: false,
-                    pointBorderColor: '#E040FB',
-                    pointBackgroundColor: '#FFF',
-                    pointHoverBackgroundColor: '#9C27B0',
-                    pointBorderWidth: 1,
-                    pointBorderRadius: 4,
-                    borderColor: '#E040FB',
-                    pointHoverBorderColor: '#212121',
-                    pointHoverBorderWidth: 1.5,
-                    pointRadius: 2.5,
-                    pointHitRadius: 8
-                }
-            ]
-};
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines} from 'react-vis';
 
-const amountAxis = [{
-    ticks: {
-        // cartesian axis eg. [2,4]
-        // chart.js docs
-        callback: (value, index, values) => {
-            return '€' + value.toFixed(2); // fixed num 2 decimals
-        }
-    }
-}]
+// // gather chartjs data
+// const data = {
+//     datasets: [
+//                 {
+//                     // styling for line chart using chartjs api props
+//                     label: 'Filtered Expenses',
+//                     showLine: true, // draw line relating to the data
+//                     fill: false,
+//                     pointBorderColor: '#E040FB',
+//                     pointBackgroundColor: '#FFF',
+//                     pointHoverBackgroundColor: '#9C27B0',
+//                     pointBorderWidth: 1,
+//                     pointBorderRadius: 4,
+//                     borderColor: '#E040FB',
+//                     pointHoverBorderColor: '#212121',
+//                     pointHoverBorderWidth: 1.5,
+//                     pointRadius: 2.5,
+//                     pointHitRadius: 8
+//                 }
+//             ]
+// };
 
-// Used to display the time from date variable
-const dateAxis = [{
-    type: 'time',
-    distribution: 'linear', // data is spread according to it's time
-    time: {
-        displayFormats: {
-            day: 'D MMM' // reading from moment.js -- chartjs 2 is dependent on moment for time
-        },
-        unit: 'day'
-    }
-}]
+// const amountAxis = [{
+//     ticks: {
+//         // cartesian axis eg. [2,4]
+//         // chart.js docs
+//         callback: (value, index, values) => {
+//             return '€' + value.toFixed(2); // fixed num 2 decimals
+//         }
+//     }
+// }]
 
-const options = {
-    legend: {
-        display: true,
-        position: top,
-        labels: {
-            fontFamily: 'Helvetica',
-            fontSize: 10
-        }
-    },
-    scales: {
-        xAxes: [{
-            ticks: {
-                autoSkip: false
-            }
-        }],
-        yAxes: amountAxis
-    }
-};
+// // Used to display the time from date variable
+// const dateAxis = [{
+//     type: 'time',
+//     distribution: 'linear', // data is spread according to it's time
+//     time: {
+//         displayFormats: {
+//             day: 'D MMM' // reading from moment.js -- chartjs 2 is dependent on moment for time
+//         },
+//         unit: 'day'
+//     }
+// }]
+
+// const options = {
+//     legend: {
+//         display: true,
+//         position: top,
+//         labels: {
+//             fontFamily: 'Helvetica',
+//             fontSize: 10
+//         }
+//     },
+//     scales: {
+//         xAxes: [{
+//             ticks: {
+//                 autoSkip: false
+//             }
+//         }],
+//         yAxes: amountAxis
+//     }
+// };
+
+const data = [
+    {x: 0, y: 8},
+    {x: 1, y: 5},
+    {x: 2, y: 4},
+    {x: 3, y: 9},
+    {x: 4, y: 1},
+    {x: 5, y: 7},
+    {x: 6, y: 6},
+    {x: 7, y: 3},
+    {x: 8, y: 2},
+    {x: 9, y: 0}
+  ];
 
 export class ExpLineChart extends React.Component {
+    
 
     render() {
         return (
-            <div>
-                Hello Expense Line Chart
+            <div className="App">
+                <XYPlot height={500} width={900}>
+                <VerticalGridLines/>
+                <HorizontalGridLines/>
+                    <XAxis/>
+                    <YAxis/>
+                    <LineSeries data={data} color="#f224f2"/>
+                </XYPlot>
             </div>
         )
     }
